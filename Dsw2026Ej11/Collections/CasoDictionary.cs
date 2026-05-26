114565bc7f925f918790ae894de863d5b10cf345
valebugeau <96093630+valebugeau@users.noreply.github.com>
2026-05-25 22:43:09 -0300

    Add methods for managing student dictionary

--- Dsw2026Ej11/Collections/CasoDictionary.cs
+++ Dsw2026Ej11/Collections/CasoDictionary.cs
@@ -1,10 +1,35 @@
-﻿namespace Dsw2026Ej11.Collections;
+using Dsw2026Ej11.Domain;
 
-//Crear un diccionario donde la clave sea el legajo y el valor el alumno
-//Incluir un método para agregar un alumno al diccionario
-//Incluir un método para buscar un alumno utilizando la clave
-//Incluir un método para retornar el diccionario
-//Incluir un método para eliminar un alumno utilizando la clave
+namespace Dsw2026Ej11.Collections;
+
 public class CasoDictionary
 {
+    //Crear un diccionario donde la clave sea el legajo (Id) y el valor el alumno
+    private Dictionary<int, Alumno> _diccionarioAlumnos = new Dictionary<int, Alumno>();
+
+    //Incluir un método para agregar un alumno al diccionario
+    public void AgregarAlumno(Alumno alumno)
+    {
+        //Id como legajo/clave
+        _diccionarioAlumnos[alumno.Id] = alumno;
+    }
+
+    //Incluir un método para buscar un alumno utilizando la clave
+    public Alumno? BuscarPorLegajo(int legajo)
+    {
+        _diccionarioAlumnos.TryGetValue(legajo, out var alumno);
+        return alumno;
+    }
+
+    //Incluir un método para retornar el diccionario
+    public Dictionary<int, Alumno> ObtenerDiccionario()
+    {
+        return _diccionarioAlumnos;
+    }
+
+    //Incluir un método para eliminar un alumno utilizando la clave
+    public void EliminarPorLegajo(int legajo)
+    {
+        _diccionarioAlumnos.Remove(legajo);
+    }
 }
